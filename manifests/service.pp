94b7069633a5eb6a8f0dd3d2060d9702050440b6
Henry <h.paulissen@tmg.nl>
2017-10-13 13:24:28 +0200

    Added systemd service-reload
    Signed-off-by: Henry <h.paulissen@tmg.nl>

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,10 +8,16 @@ class thumbor::service
     owner   => $thumbor::user,
     group   => $thumbor::group,
     mode    => '0644',
-    content => template('thumbor/thumbor.systemd.erb')
+    content => template('thumbor/thumbor.systemd.erb'),
+    notify  => Exec['thumbor-systemd-reload'],
   }
   -> thumbor::service::systemd{ $thumbor::ports: }
   -> anchor { 'thumbor::service::end': }
+
+  exec { 'thumbor-systemd-reload':
+    command => 'systemctl daemon-reload',
+    path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin' ],
+  }
 }
 
 define thumbor::service::systemd
@@ -23,6 +29,7 @@ define thumbor::service::systemd
     command => "systemctl enable thumbor@${port}",
     creates => "/etc/systemd/system/multi-user.target.wants/thumbor@${port}.service",
     path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin' ],
+    notify  => Exec['thumbor-systemd-reload'],
   }
 
   service { "thumbor@${port}":
